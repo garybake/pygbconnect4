3489b74108fd4b62241a8aebe63e73426f435768
Gary Bake <garybake@gmail.com>
2019-01-24 23:36:21 +0000

    Can play on board directly

---
+++ connect4/c4board.py
@@ -0,0 +1,69 @@
+from connect4.c4types import Point
+
+
+class Move():
+    """
+    User can choose which column to play
+
+    Move.play(col)
+    """
+    def __init__(self, col=None):
+        assert col is not None
+        self.col = col
+
+    @classmethod
+    def play(cls, col):
+        """
+        Places a stone on the board
+        """
+        return Move(col=col)
+
+
+class Board():
+    """
+    Holds a game board in the current state
+    """
+
+    def __init__(self, num_rows, num_cols):
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self._grid = {}  # dict of points
+
+    def place_piece(self, player, col):
+        high = self.find_highest_empty(col)
+        if high is None:
+            print('No room in column {}'.format(col))
+            return False
+        else:
+            point = Point(high, col)
+            print('Applying at {}'.format(point))
+            self.apply_move(point, player)
+
+    def find_highest_empty(self, col):
+        for row in range(self.num_rows, 0, -1):
+            potential_point = Point(row, col)
+            if self.get(potential_point):
+                continue
+            else:
+                return row
+        return None
+
+    def get(self, point):
+        """
+        Returns content of point on the board
+        Player if stone is on that point
+        else None
+        """
+        player = self._grid.get(point)
+        if player is None:
+            return None
+        return player
+
+    def apply_move(self, point, player):
+        grid = self._grid
+        if grid.get(point):
+            print('Piece already at {}'.format(point))
+            return False
+        else:
+            grid[point] = player
+            return True
---
+++ connect4/utils.py
@@ -0,0 +1,25 @@
+from connect4 import c4types
+
+COLS = 'ABCDEFGHIJKLMNOPQRST'
+STONE_TO_CHAR = {
+    None: ' . ',
+    c4types.Player.yellow: ' Y ',
+    c4types.Player.red: ' R '
+}
+
+
+def print_move(player, move):
+    move_str = '%s%d' % (COLS[move.point.col - 1], move.point.row)
+
+    print('%s %s' % (player, move_str))
+
+
+def print_board(board):
+    for row in range(1, board.num_rows + 1):
+        bump = " " if row <= 9 else ""
+        line = []
+        for col in range(1, board.num_cols + 1):
+            stone = board.get(c4types.Point(row=row, col=col))
+            line.append(STONE_TO_CHAR[stone])
+        print('%s%d %s' % (bump, row, ''.join(line)))
+    print('    ' + '  '.join(COLS[:board.num_cols]))
